ec88f76e4af0ce33565eb691cd9fb3e9a3ce59c2
harangji <radio000507@gmail.com>
2025-05-16 17:48:23 +0900

    AddJoyStick

---
+++ Assets/3.Scripts/OutGame/TouchManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine.InputSystem.Controls;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+    public static TouchManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public Action<TouchControl> OnTouchStart;
+    public Action<TouchControl> OnTouchMove;
+    public Action<TouchControl> OnTouchEnd;
+
+    private void Update()
+    {
+        if (Touchscreen.current == null)
+            return;
+
+        foreach (var touch in Touchscreen.current.touches)
+        {
+            if (touch.phase.ReadValue() == UnityEngine.InputSystem.TouchPhase.Began)
+            {
+                OnTouchStart?.Invoke(touch);
+            }
+            else if (touch.phase.ReadValue() == UnityEngine.InputSystem.TouchPhase.Moved)
+            {
+                OnTouchMove?.Invoke(touch);
+            }
+            else if (touch.phase.ReadValue() == UnityEngine.InputSystem.TouchPhase.Ended ||
+                     touch.phase.ReadValue() == UnityEngine.InputSystem.TouchPhase.Canceled)
+            {
+                OnTouchEnd?.Invoke(touch);
+            }
+        }
+    }
+}
