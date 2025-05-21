859f06d77c94cea69f2a8b0801dd6a8974dc4f2a
harangji <radio000507@gmail.com>
2025-05-21 09:58:37 +0900

    Delete Shapes, Project Settings

--- Assets/3.Scripts/OutGame/SceneControllers/MainMenu_OutGame/MainMenuManager.cs
+++ Assets/3.Scripts/OutGame/SceneControllers/MainMenu_OutGame/MainMenuManager.cs
@@ -13,19 +13,23 @@ public class MainMenuManager : MonoBehaviour
     private const string EmailKey = "UserEmail";
     private const string PasswordKey = "UserPassword";
     
+    private bool isLoginInProgress = false; //로그인 중복 시도를 막는 bool변수
+    
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isLoginInProgress.Equals(false)) // any pressed
         {
-            if (PlayerPrefs.HasKey(EmailKey) && PlayerPrefs.HasKey(PasswordKey))
+            if (PlayerPrefs.HasKey(EmailKey) && PlayerPrefs.HasKey(PasswordKey)) // 자동 로그인 트리거
             {
+                isLoginInProgress = true; //중복실행을 막음
+                
                 string savedEmail = PlayerPrefs.GetString(EmailKey);
                 string savedPassword = PlayerPrefs.GetString(PasswordKey);
 
                 // 자동 로그인 시도
                 AutoLogin(savedEmail, savedPassword);
             }
-            else
+            else // 자동 로그인 실패
             {
                 popupLogin.gameObject.SetActive(true);
             }
