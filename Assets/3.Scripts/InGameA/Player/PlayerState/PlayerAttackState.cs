8dd7fe9abacaa04fab420545ddeafc745be8bc76
TaeGun08 <gustn4984@naver.com>
2025-05-21 16:01:02 +0900

    Attack 애니메이션 재생 및 딜레이 추가
    Coroutine을 활용하여 지연시간을 줬음
    지연시간 후 Idle 상태로 전환되록 하였음

--- Assets/3.Scripts/InGameA/Player/PlayerState/PlayerAttackState.cs
+++ Assets/3.Scripts/InGameA/Player/PlayerState/PlayerAttackState.cs
@@ -9,15 +9,22 @@ public class PlayerAttackState : PlayerState
     public override void StateEnter(PlayerController playerController)
     {
         this.playerController = playerController;
+        this.playerController.ChangeAnimation(CurrentState);
+        StartCoroutine(AttackCoroutine());
+    }
+
+    private IEnumerator AttackCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        playerController.ChangeState(State.Idle);
     }
 
     public override void StateUpdate()
     {
-        playerController.ChangeState(State.Idle);
     }
 
     public override void StateExit()
     {
-
+        playerController.ResetAnimation(CurrentState);
     }
 }
