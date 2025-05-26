c4589a4c6dbbd31fe8aaccf79650b91b1a1f3fcb
lilill16171df <luna17f@naver.com>
2025-05-26 17:08:43 +0900

    전적씬 작업

---
+++ Assets/3.Scripts/Network/UI/RecordUI.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RecordUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text exp;
+    [SerializeField] private TMP_Text gold;
+    // [SerializeField] private TMP_Text gold;
+
+    private void Start()
+    {
+        // FirestoreManager.Instance.ReadDataAsync<>()
+    }
+}
