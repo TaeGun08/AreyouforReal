ef024dfc748f772b61429e1b1ffa96bff5f0ba46
TaeGun08 <gustn4984@naver.com>
2025-05-26 14:29:18 +0900

    캐릭터 이동 버그 수정

--- Assets/3.Scripts/InGameA/Player/PlayerController.cs
+++ Assets/3.Scripts/InGameA/Player/PlayerController.cs
@@ -13,18 +13,20 @@ public class PlayerController : NetworkBehaviour, IKnockout
     public NetworkCharacterController CharacterController => characterController;
 
     public NetworkObject NetworkObj => Object;
-    
+
     private Animator animator;
-    
+
     [SerializeField] private PlayerState[] playerStates;
     private PlayerState currentState;
-    
-    private Dictionary<PlayerState.State, PlayerState> playerStateDic = new Dictionary<PlayerState.State, PlayerState>();
+
+    private Dictionary<PlayerState.State, PlayerState>
+        playerStateDic = new Dictionary<PlayerState.State, PlayerState>();
+
     private Dictionary<PlayerState.State, int> playerAnimDic = new Dictionary<PlayerState.State, int>();
-    
+
     // 킬로그용
     public static event Action<PlayerController, PlayerController> OnPlayerKnockoutEvent;
-    
+
     private void Awake()
     {
         localPlayer = GetComponent<LocalPlayer>();
@@ -36,7 +38,7 @@ public class PlayerController : NetworkBehaviour, IKnockout
             playerStateDic.Add(playerStates[i].CurrentState, playerStates[i]);
             playerAnimDic.Add(playerStates[i].CurrentState, Animator.StringToHash($"{playerStates[i].CurrentState}"));
         }
-        
+
         ChangeState(PlayerState.State.Idle);
         animator.ResetTrigger(playerAnimDic[PlayerState.State.Idle]);
     }
@@ -45,26 +47,26 @@ public class PlayerController : NetworkBehaviour, IKnockout
     {
         currentState?.StateUpdate();
     }
-    
+
     public void ChangeState(PlayerState.State newState)
     {
         if (currentState != null)
-            if (currentState.CurrentState.Equals(newState)) return;
-        
+            if (currentState.CurrentState.Equals(newState))
+                return;
+
         currentState?.StateExit();
-        
+
         currentState = playerStateDic[newState];
-        
+
         currentState.StateEnter(this);
     }
 
     [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
     public void RPC_Knockout()
     {
-        
         ChangeState(PlayerState.State.Knockout);
     }
-    
+
     public void ChangeAnimation(PlayerState.State newState)
     {
         animator.SetTrigger(playerAnimDic[newState]);
@@ -74,16 +76,16 @@ public class PlayerController : NetworkBehaviour, IKnockout
     {
         animator.ResetTrigger(playerAnimDic[curState]);
     }
-    
+
     // 킬로그용 영돈
-   // public void RPC_PlayerKnockout(NetworkObjectRef attackerRef)
-   // {
-   //     ChangeState(PlayerState.State.Knockout);
-   //     ChangeAnimation(PlayerState.State.Knockout);
+    // public void RPC_PlayerKnockout(NetworkObjectRef attackerRef)
+    // {
+    //     ChangeState(PlayerState.State.Knockout);
+    //     ChangeAnimation(PlayerState.State.Knockout);
 //
-   //     var attackerObj = Runner.GetPlayerObject(attackerRef)?
-   //         .GetComponent<PlayerController>();
-   //     if (attackerObj != null)
-   //         OnPlayerKnockoutEvent?.Invoke(attackerObj, this);
-   // }
+    //     var attackerObj = Runner.GetPlayerObject(attackerRef)?
+    //         .GetComponent<PlayerController>();
+    //     if (attackerObj != null)
+    //         OnPlayerKnockoutEvent?.Invoke(attackerObj, this);
+    // }
 }
