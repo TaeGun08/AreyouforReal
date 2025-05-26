cfcd2546e98f06d7ace295f8ef75b8162806491a
lilill16171df <luna17f@naver.com>
2025-05-26 14:21:01 +0900

    AIManager, GameManager 작업

---
+++ Assets/3.Scripts/Network/Manangers/AIManager.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+
+public class AIManager : NetworkBehaviour
+{
+    public static AIManager Instance;
+    
+    [SerializeField] private NetworkPrefabRef AIPrefab;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SpawnAI(Vector3 pos)
+    {
+        Debug.Assert(Runner.IsServer, "서버전용");
+        
+        // TODO : AI 안움직이면 InputAuth 한번 체크해봐야할듯
+        Runner.Spawn(AIPrefab, pos, Quaternion.identity);
+    }
+
+    public void DespawnAI(NetworkObject networkObject)
+    {
+        Debug.Assert(Runner.IsServer, "서버전용");
+        Runner.Despawn(networkObject);
+    }
+}
--- Assets/3.Scripts/Network/Manangers/GameManager_Network.cs
+++ Assets/3.Scripts/Network/Manangers/GameManager_Network.cs
@@ -15,6 +15,10 @@ public class GameManager_Network : NetworkBehaviour
     
     [Networked, Capacity(MAX_PLAYER)]
     public NetworkLinkedList<PlayerRef> AlivePlayers { get; }
+    
+    [Networked]
+    [field: SerializeField]
+    private int aliveAICount { get; set; } 
 
     [SerializeField] private GameObject map;
     [Networked] private GameState delayedState { get; set; }
@@ -53,14 +57,16 @@ public class GameManager_Network : NetworkBehaviour
         
         if (2 <= PlayerRegistry.Instance.playerDic.Count)
         {
-            // TODO : 로딩화면 활성화
-            
+            // TODO : 로딩화면 활성화, AI 스폰, 자기장
+            // AIManager.Instance.SpawnAI(
+            //     TelpoTransform.Instance.TelepoTrs[Random.Range(0, TelpoTransform.Instance.TelepoTrs.Length)].position);
             RPC_MapActive();
             state = GameState.Start;
             
             foreach (var player in PlayerRegistry.Instance.playerDic)
             {
                 Transform trs = TelpoTransform.Instance.TelepoTrs[Random.Range(0, TelpoTransform.Instance.TelepoTrs.Length)];
+                
                 // 캐싱필요
                 player.Value.GetComponent<NetworkCharacterController>().Teleport(trs.position);
                 AlivePlayers.Add(player.Key);
@@ -88,6 +94,27 @@ public class GameManager_Network : NetworkBehaviour
 
     public void KillEvent(PlayerRef player)
     {
-        AlivePlayers.Remove(player);
+        if (AlivePlayers.Remove(player))
+        {
+            if (AlivePlayers.Count <= 1)
+            {
+                EndGame();
+            }
+        }
+    }
+
+    private void EndGame()
+    {
+        state = GameState.End;
+        
+        // TODO : 전적표시
+        
+        foreach (var player in PlayerRegistry.Instance.playerDic)
+        {
+            // 라운지로 이동
+            player.Value.GetComponent<NetworkCharacterController>().Teleport(new Vector3(0, 2, 0));
+            AlivePlayers.Add(player.Key);
+        }
+        
     }
 }
