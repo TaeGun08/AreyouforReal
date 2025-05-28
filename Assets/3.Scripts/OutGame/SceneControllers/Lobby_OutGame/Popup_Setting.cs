70fca370c5271f76faf51be7e5d77389d0de1945
harangji <68278198+harangji@users.noreply.github.com>
2025-05-29 00:03:21 +0900

    BGM 리소스 추가 - 전부 Mono로 변환 완료 세팅에서 볼륨조절 준비

--- Assets/3.Scripts/OutGame/SceneControllers/Lobby_OutGame/Popup_Setting.cs
+++ Assets/3.Scripts/OutGame/SceneControllers/Lobby_OutGame/Popup_Setting.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Popup_Setting : BaseWindow
 {
+    // [Header("UI Components")]
+    // public Slider masterVolumeSlider;
+    //
+    // private void Start()
+    // {
+    //     // 슬라이더 초기값 세팅 (저장된 값 불러오기)
+    //     float savedVolume = PlayerPrefs.GetFloat("MasterVolume", 1f);
+    //     masterVolumeSlider.value = savedVolume;
+    //
+    //     // 슬라이더 변경 이벤트 연결
+    //     masterVolumeSlider.onValueChanged.AddListener(OnMasterVolumeChanged);
+    // }
+    //
+    // private void OnMasterVolumeChanged(float value)
+    // {
+    //     // 저장
+    //     PlayerPrefs.SetFloat("MasterVolume", value);
+    //     PlayerPrefs.Save();
+    //
+    //     // 사운드 매니저에 전달
+    //     SoundManager_OutGame.Instance.SetMasterVolume(value);
+    // }
+    
     public void OnClickedUserIdButton()
     {
         GUIUtility.systemCopyBuffer = FirebaseMainSession.Instance.FirebaseUser.UserData.UserId;
